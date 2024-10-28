eb25830c3d985a9b346c3272778562ff634cebe6
SebitasDev <sramirezg13@eafit.edu.co>
2024-10-27 19:36:21 -0500

    fix: imports solved

--- src/main/java/com/nodo/retobackend/repository/IUserRepository.java
+++ src/main/java/com/nodo/retobackend/repository/IUserRepository.java
@@ -3,6 +3,7 @@ package com.nodo.retobackend.repository;
 import com.nodo.retobackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, String> {
--- src/main/java/com/nodo/retobackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/nodo/retobackend/service/impl/UserServiceImpl.java
@@ -3,29 +3,29 @@ package com.nodo.retobackend.service.impl;
 import com.nodo.retobackend.dto.ResponseDto;
 import com.nodo.retobackend.dto.UserRequestDto;
 import com.nodo.retobackend.model.User;
-import com.nodo.retobackend.repository.UserRepository;
+import com.nodo.retobackend.repository.IUserRepository;
 import com.nodo.retobackend.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.nodo.retobackend.mapper.UserMapper;
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements IUserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private IUserRepository _userRepository;
 
     @Override
     public ResponseDto<Boolean> register(UserRequestDto payload) {
-        Optional<User> existingUser = userRepository.findByMail(payload.getMail());
+        Optional<User> existingUser = _userRepository.findByMail(payload.getMail());
         if (existingUser.isPresent()) {
-            return new ResponseDto<>(false, "Email already registered", false);
+            return new ResponseDto<>(400, "Email already registered", false);
         }
 
         User user = UserMapper.INSTANCE.toUser(payload);
 
-        userRepository.save(user);
+        _userRepository.save(user);
 
         return new ResponseDto<>(201, "User registered successfully", true);
     }
