a7588162a14e11bcb012dafb14304024f3808c24
Diana <dianaroldanr162@gmail.com>
2024-11-13 22:51:11 -0500

    se modifica funcionalidad para persistir las respuestas

--- src/main/java/com/nodo/retobackend/repository/IAnswerRepository.java
+++ src/main/java/com/nodo/retobackend/repository/IAnswerRepository.java
@@ -1,9 +1,28 @@
 package com.nodo.retobackend.repository;
 
 import com.nodo.retobackend.model.Answer;
+import com.nodo.retobackend.model.QuestionOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface IAnswerRepository extends JpaRepository<Answer, Long> {
+    @Query("SELECT a FROM Answer a WHERE a.user.documentNumber = :userDocument AND a.questionOption.question.id = :questionId")
+    Optional<Answer> findByUserDocumentAndQuestionId(@Param("userDocument") String userDocument, @Param("questionId") Long questionId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO answers(id_question_option, id_user) VALUES(:questionOptionId, :userId)", nativeQuery = true)
+    void saveNative(@Param("questionOptionId") Long questionOptionId, @Param("userId") String userId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Answer a SET a.questionOption = :questionOption WHERE a.id = :id")
+    void updateQuestionOption(@Param("questionOption") QuestionOption questionOption, @Param("id") Long id);
 }
